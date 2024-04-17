fd0b791d550533cee68e90a789995c0b69d6cce2
Pedro Melo <58223549+lolitopt2@users.noreply.github.com>
2024-04-17 22:50:37 +0100

    Adicionar arquivos de projeto.

---
+++ ProjetoVet/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace ProjetoVet
+{
+    partial class Vet
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Vet));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tipotxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pesoTxt = new System.Windows.Forms.TextBox();
+            this.sexoTxt = new System.Windows.Forms.TextBox();
+            this.Racatxt = new System.Windows.Forms.TextBox();
+            this.dataConsutxt = new System.Windows.Forms.TextBox();
+            this.dataNascTxt = new System.Windows.Forms.TextBox();
+            this.nomeDonoTxt = new System.Windows.Forms.TextBox();
+            this.nomeTxt = new System.Windows.Forms.TextBox();
+            this.idTxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(801, 452);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(795, 427);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ficha Animais";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tipotxt);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.pesoTxt);
+            this.groupBox2.Controls.Add(this.sexoTxt);
+            this.groupBox2.Controls.Add(this.Racatxt);
+            this.groupBox2.Controls.Add(this.dataConsutxt);
+            this.groupBox2.Controls.Add(this.dataNascTxt);
+            this.groupBox2.Controls.Add(this.nomeDonoTxt);
+            this.groupBox2.Controls.Add(this.nomeTxt);
+            this.groupBox2.Controls.Add(this.idTxt);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(510, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(261, 359);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // tipotxt
+            // 
+            this.tipotxt.Location = new System.Drawing.Point(56, 278);
+            this.tipotxt.Name = "tipotxt";
+            this.tipotxt.Size = new System.Drawing.Size(137, 20);
+            this.tipotxt.TabIndex = 38;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 280);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 13);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "Tipo";
+            // 
+            // pesoTxt
+            // 
+            this.pesoTxt.Location = new System.Drawing.Point(56, 252);
+            this.pesoTxt.Name = "pesoTxt";
+            this.pesoTxt.Size = new System.Drawing.Size(137, 20);
+            this.pesoTxt.TabIndex = 36;
+            // 
+            // sexoTxt
+            // 
+            this.sexoTxt.Location = new System.Drawing.Point(56, 227);
+            this.sexoTxt.Name = "sexoTxt";
+            this.sexoTxt.Size = new System.Drawing.Size(137, 20);
+            this.sexoTxt.TabIndex = 35;
+            // 
+            // Racatxt
+            // 
+            this.Racatxt.Location = new System.Drawing.Point(58, 203);
+            this.Racatxt.Name = "Racatxt";
+            this.Racatxt.Size = new System.Drawing.Size(137, 20);
+            this.Racatxt.TabIndex = 34;
+            // 
+            // dataConsutxt
+            // 
+            this.dataConsutxt.Location = new System.Drawing.Point(96, 180);
+            this.dataConsutxt.Name = "dataConsutxt";
+            this.dataConsutxt.Size = new System.Drawing.Size(137, 20);
+            this.dataConsutxt.TabIndex = 33;
+            // 
+            // dataNascTxt
+            // 
+            this.dataNascTxt.Location = new System.Drawing.Point(80, 157);
+            this.dataNascTxt.Name = "dataNascTxt";
+            this.dataNascTxt.Size = new System.Drawing.Size(137, 20);
+            this.dataNascTxt.TabIndex = 32;
+            // 
+            // nomeDonoTxt
+            // 
+            this.nomeDonoTxt.Location = new System.Drawing.Point(104, 132);
+            this.nomeDonoTxt.Name = "nomeDonoTxt";
+            this.nomeDonoTxt.Size = new System.Drawing.Size(137, 20);
+            this.nomeDonoTxt.TabIndex = 31;
+            // 
+            // nomeTxt
+            // 
+            this.nomeTxt.Location = new System.Drawing.Point(94, 106);
+            this.nomeTxt.Name = "nomeTxt";
+            this.nomeTxt.Size = new System.Drawing.Size(137, 20);
+            this.nomeTxt.TabIndex = 30;
+            // 
+            // idTxt
+            // 
+            this.idTxt.Location = new System.Drawing.Point(42, 86);
+            this.idTxt.Name = "idTxt";
+            this.idTxt.Size = new System.Drawing.Size(137, 20);
+            this.idTxt.TabIndex = 29;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 252);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Peso";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Sexo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Raça";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "DataConsulta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Nome do Dono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "DataNasc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Nome Animal";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Id";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-15, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 427);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(496, 382);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 42);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Inserir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(200, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Atualizar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Apagar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Blue;
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(793, 426);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Consulta";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(588, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(782, 369);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Vet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Vet";
+            this.Text = "Veterinário";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox pesoTxt;
+        private System.Windows.Forms.TextBox sexoTxt;
+        private System.Windows.Forms.TextBox Racatxt;
+        private System.Windows.Forms.TextBox dataConsutxt;
+        private System.Windows.Forms.TextBox dataNascTxt;
+        private System.Windows.Forms.TextBox nomeDonoTxt;
+        private System.Windows.Forms.TextBox nomeTxt;
+        private System.Windows.Forms.TextBox idTxt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tipotxt;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ ProjetoVet/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ProjetoVet
+{
+    public partial class Vet : Form
+    {
+        public Vet()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(" Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\pedro\\source\\repos\\ProjetoVet\\ProjetoVet\\DBAnimais.mdf; Integrated Security = True\r\n");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Animais values (@Id,@nome,@dataNasc,@dataUltiConsu,@tipo,@raca,@sexo,@peso)", con);
+            cmd.Parameters.AddWithValue("@Id", int.Parse(idTxt.Text));
+            cmd.Parameters.AddWithValue("@nome", pesoTxt.Text);
+            cmd.Parameters.AddWithValue("@dataNasc", dataNascTxt.Text);
+            cmd.Parameters.AddWithValue("@dataUltiConsu", dataConsutxt.Text);
+            cmd.Parameters.AddWithValue("@tipo", tipotxt.Text);
+            cmd.Parameters.AddWithValue("@raca", Racatxt.Text);
+            cmd.Parameters.AddWithValue("@sexo", sexoTxt.Text);
+            cmd.Parameters.AddWithValue("@peso", pesoTxt.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Dados do Bicho adicionados");
+        }
+
+
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(" Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\pedro\\source\\repos\\ProjetoVet\\ProjetoVet\\DBAnimais.mdf; Integrated Security = True\r\n");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete Animais where Id = @Id", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(idTxt.Text));
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Apagado com Sucesso");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pedro\\source\\repos\\VendasProje\\VendasProje\\Database1.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update Clientes set nome= @nome, email= @email, nif= @nif, profissao= @profissao where Id = @Id", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(idTxt.Text));
+            cmd.Parameters.AddWithValue("@nome", nomeTxt.Text);
+           //Restantes Campos
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Atualizado com Sucesso");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pedro\\source\\repos\\VendasProje\\VendasProje\\Database1.mdf;Integrated Security=True");
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+            }
+            SqlCommand cmd1 = new SqlCommand("select * from Animais where Id like '%" + idTxt.Text + "%'", con);
+            cmd1.Parameters.AddWithValue("@ID", int.Parse(idTxt.Text));
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            da.SelectCommand = cmd1;
+            dt.Clear();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+    }
+
+
+
+
+    }
+
